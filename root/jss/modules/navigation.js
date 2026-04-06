508ca57ce0a963f803227e1a37e3fcaf7b69a88c
skyhightech23 <skylar@skyhightechsolutions.net>
2026-04-05 22:23:44 -0500

    adding css

---
+++ root/jss/modules/navigation.js
@@ -0,0 +1,12 @@
+function goToPage(page){
+  document.querySelectorAll('.nav-item').forEach(i => {
+    if( i.dataset.page === page ){
+        i.classList.add('active');
+        nav(i);
+        
+    } else {
+        i.classList.remove('active');
+    }
+  });
+
+}
